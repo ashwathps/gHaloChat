1c3cb61a287c0a7b0f39879a86b7f12a60be3b3e
ashwath <ashwathps@users.noreply.github.com>
2014-06-28 10:31:11 +0530

    code comments

--- public/app/ngResources/listOfUsersRes.js
+++ public/app/ngResources/listOfUsersRes.js
@@ -1,4 +1,10 @@
+/*
+  $resurce request to fetch all the registered users in the server.
+  limit the response to 10 per request
+*/
+
 angular.module('app').factory('listOfUsersRes', ['$resource', function($resource){
+  //TODO: handle limit
   var listOfUsersResource =  $resource('/api/all_users/:uid', {uid: '@uid', limit: 10}, {
       query: {method: 'GET', isArray:true}
   });
